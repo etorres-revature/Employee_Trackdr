6e8120257dd984aa025269d5c7459af18082c855
Eric D. Torres <etorresnotary@gmail.com>
2020-10-07 22:05:54 -0500

    create connection to MySQL database

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 
+//create/configure connection to the MySQL database
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "",
     database: "company_db"
-})
+});
+
+//connect to the MySQL server, and call for inquirere prompts when connected
+connection.connect(err => {
+    if (err) throw err;
+    inquirerPrompt();
+});
