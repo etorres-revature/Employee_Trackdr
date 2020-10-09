ac223d7f2d7c565b06b372a75c041a6f8744cb0e
Eric D. Torres <etorresnotary@gmail.com>
2020-10-08 23:12:15 -0500

    all functionality workinggit add .

--- assets/lib/employee.js
+++ assets/lib/employee.js
@@ -5,184 +5,186 @@ const inquireMod = require("./inquirerPrompts");
 const inquirer = require("inquirer");
 
 function viewEmployees() {
-    const query = "SELECT * FROM employee";
-    connection.query(query, (err, res) => {
-      if (err) throw err;
-      const table = cTable.getTable(res);
-      console.log(table);
-      inquireMod.prompts();
+  const query = "SELECT * FROM employee";
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    const table = cTable.getTable(res);
+    console.log(table);
+    inquireMod.prompts();
+  });
+}
+
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        name: "empFName",
+        type: "input",
+        message: "Please input new employee's first name.",
+      },
+      {
+        name: "empLName",
+        type: "input",
+        message: "Please input the new employee's last name.",
+      },
+      {
+        name: "empRole",
+        type: "input",
+        message: "Please input the Role ID for the new employee.",
+      },
+      {
+        name: "empManager",
+        type: "input",
+        message:
+          "Please input Employee ID of the new employee (Please leave blank if the employee is a manager).",
+      },
+    ])
+    .then((data) => {
+      const query = "INSERT INTO employee SET ?";
+      const newEmp = {
+        first_name: data.empFName,
+        last_name: data.empLName,
+        role_id: data.empRole,
+        manager_id: data.empManager,
+      };
+      connection.query(query, newEmp, (err, res) => {
+        if (err) throw err;
+        console.log("New Employee successfully added.");
+        inquireMod.prompts();
+      });
     });
-  }
-  
-  
-  function addEmployee() {
-    inquirer
-      .prompt([
+}
+
+function updateManager() {
+  inquirer
+    .prompt([
+      {
+        name: "employee",
+        type: "input",
+        message:
+          "Enter the ID of the Employee whose Manager you would like to update.",
+      },
+      {
+        name: "manager",
+        type: "input",
+        message: "Enter the new Manager ID for this Employee.",
+      },
+    ])
+    .then((data) => {
+      const query = "UPDATE employee SET ? WHERE ?";
+      const newRole = [
         {
-          name: "empFName",
-          type: "input",
-          message: "Please input new employee's first name.",
+          manager_id: data.manager,
         },
         {
-          name: "empLName",
-          type: "input",
-          message: "Please input the new employee's last name.",
+          id: data.employee,
         },
-        {
-          name: "empRole",
-          type: "input",
-          message: "Please input the Role ID for the new employee.",
-        },
-        {
-          name: "empManager",
-          type: "input",
-          message:
-            "Please input Employee ID of the new employee (Please leave blank if the employee is a manager).",
-        },
-      ])
-      .then((data) => {
-        const query = "INSERT INTO employee SET ?";
-        const newEmp = {
-          first_name: data.empFName,
-          last_name: data.empLName,
-          role_id: data.empRole,
-          manager_id: data.empManager,
-        };
-        connection.query(query, newEmp, (err, res) => {
-          if (err) throw err;
-          console.log("New Employee successfully added.");
-          inquireMod.prompts();
-        });
+      ];
+      connection.query(query, newRole, (err, res) => {
+        if (err) throw err;
+        inquireMod.prompts();
       });
-  }
-  
-  function updateManager() {
-    inquirer
-      .prompt([
-        {
-          name: "employee",
-          type: "input",
-          message:
-            "Enter the ID of the Employee whose Manager you would like to update.",
-        },
-        {
-          name: "manager",
-          type: "input",
-          message: "Enter the new Manager ID for this Employee.",
-        },
-      ])
-      .then((data) => {
-        const query = "UPDATE employee SET ? WHERE ?";
-        const newRole = [
-          {
-            manager_id: data.manager,
-          },
-          {
-            id: data.employee,
-          },
-        ];
-        connection.query(query, newRole, (err, res) => {
-          if (err) throw err;
-          inquireMod.prompts();
-        });
-      });
-  }
-  
-  function viewEmployeesByManager() {
-    const query = `SELECT CONCAT(e2.first_name, " ", e2.last_name) AS Manager, e1.id AS EMPID, e1.first_name AS FName, e1.last_name AS LName, role.title AS Title, department.name AS Department, role.salary AS Salary FROM employee AS e1
+    });
+}
+
+function viewEmployeesByManager() {
+  const query = `SELECT CONCAT(e2.first_name, " ", e2.last_name) AS Manager, e1.id AS EMPID, e1.first_name AS FName, e1.last_name AS LName, role.title AS Title, department.name AS Department, role.salary AS Salary FROM employee AS e1
       LEFT JOIN role on e1.role_id = role.id
       LEFT JOIN department ON role.department_id = department.id
       INNER JOIN employee AS e2 on e2.id=e1.manager_id
       ORDER BY manager ASC;`;
-    connection.query(query, (err, res) => {
-      if (err) throw err;
-      const table = cTable.getTable(res);
-      console.log(table);
-      inquireMod.prompts();
-    });
-  }
-  
-  function viewEmployeesByDepartment() {
-      const query = `SELECT e1.id AS EMPID, e1.first_name AS FName, e1.last_name AS LName, role.title AS Title, department.name AS Department, role.salary AS Salary, CONCAT(e2.first_name, " ", e2.last_name) AS Manager FROM employee AS e1
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    const table = cTable.getTable(res);
+    console.log(table);
+    inquireMod.prompts();
+  });
+}
+
+function viewEmployeesByDepartment() {
+  const query = `SELECT e1.id AS EMPID, e1.first_name AS FName, e1.last_name AS LName, role.title AS Title, department.name AS Department, role.salary AS Salary, CONCAT(e2.first_name, " ", e2.last_name) AS Manager FROM employee AS e1
       LEFT JOIN role on e1.role_id = role.id
       LEFT JOIN department ON role.department_id = department.id
       LEFT JOIN employee AS e2 on e2.id=e1.manager_id
       ORDER BY department ASC;`;
-    connection.query(query, (err, res) => {
-      if (err) throw err;
-      const table = cTable.getTable(res);
-      console.log(table);
-      inquireMod.prompts();
-    });
-  }
-
-  function deleteEmployee() {
-    inquirer
-      .prompt([
-        {
-          name: "empID",
-          type: "input",
-          message: "Enter the ID of the Employee you would like to delete.",
-        },
-      ])
-      .then((data) => {
-        const query = "DELETE FROM employee WHERE ?";
-        const deleteEmp = {
-          id: data.empID
-        };
-        connection.query(query, deleteEmp, (err, res) => {
-          if (err) throw err;
-          console.log("This Employee has been deleted from the company records.");
-          inquireMod.prompts();
-        });
-      });
-  }
-
-  function viewCompanyBudget() {
-    const query = `SELECT department.name AS Department, SUM(role.salary) AS Dept_Budget FROM employee
-    LEFT JOIN role ON employee.role_id = role.id
-    LEFT JOIN department ON role.department_id = department.id
-    GROUP BY department.name;`;
-    connection.query(query, (err, res) => {
-        if (err) throw err;
-        const table = cTable.getTable(res);
-        console.log(table);
-        inquireMod.prompts(); 
-    })
-}
-
-  function viewTotalBudgetDept() {
-      inquirer.prompt([{
-          name: "deptID",
-          type: "input",
-          message: "Please enter the name of the Department to see its budget utilization."
-      }])
-    .then(data => {
-        const query = `SELECT department.name AS Department, SUM(role.salary) AS Dept_Budget FROM employee 
-        LEFT JOIN role ON employee.role_id = role.id
-        LEFT JOIN department ON role.department_id = department.id
-        WHERE department.id = ?;`;
-
-        const queryDept = {
-            id: data.deptID
-        }
-        
-  connection.query(query, queryDept, (err, res) => {
+  connection.query(query, (err, res) => {
     if (err) throw err;
     const table = cTable.getTable(res);
     console.log(table);
-    inquireMod.prompts(); 
-  })
-  })
+    inquireMod.prompts();
+  });
+}
+
+function deleteEmployee() {
+  inquirer
+    .prompt([
+      {
+        name: "empID",
+        type: "input",
+        message: "Enter the ID of the Employee you would like to delete.",
+      },
+    ])
+    .then((data) => {
+      const query = "DELETE FROM employee WHERE ?";
+      const deleteEmp = {
+        id: data.empID,
+      };
+      connection.query(query, deleteEmp, (err, res) => {
+        if (err) throw err;
+        console.log("This Employee has been deleted from the company records.");
+        inquireMod.prompts();
+      });
+    });
+}
+
+function viewCompanyBudget() {
+  const query = `SELECT department.name AS Department, SUM(role.salary) AS Dept_Budget FROM employee
+    LEFT JOIN role ON employee.role_id = role.id
+    LEFT JOIN department ON role.department_id = department.id
+    GROUP BY department.name;`;
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    const table = cTable.getTable(res);
+    console.log(table);
+    inquireMod.prompts();
+  });
+}
+
+function viewTotalBudgetDept() {
+  inquirer
+    .prompt([
+      {
+        name: "deptName",
+        type: "input",
+        message:
+          "Please enter the name of the Department to see its budget utilization.",
+      },
+    ])
+    .then((data) => {
+      const query = `SELECT department.name AS Department, SUM(role.salary) AS Dept_Budget FROM employee
+      LEFT JOIN role on employee.role_id = role.id
+      LEFT JOIN department ON role.department_id = department.id
+      WHERE ?;`;
+
+      const queryDept = {
+        name: data.deptName,
+      };
+        connection.query(query, queryDept, (err, res) => {
+        if (err) throw err;
+        const table = cTable.getTable(res);
+        console.log(table);
+        inquireMod.prompts();
+      });
+    });
 }
 
 module.exports = {
-    create: addEmployee,
-    read: viewEmployees,
-    empByMan: viewEmployeesByManager,
-    empByDept: viewEmployeesByDepartment,
-    companyBudget: viewCompanyBudget, 
-    deptBudget: viewTotalBudgetDept,
-    updateMan: updateManager,
-    delete: deleteEmployee
+  create: addEmployee,
+  read: viewEmployees,
+  empByMan: viewEmployeesByManager,
+  empByDept: viewEmployeesByDepartment,
+  companyBudget: viewCompanyBudget,
+  deptBudget: viewTotalBudgetDept,
+  updateMan: updateManager,
+  delete: deleteEmployee,
 };
